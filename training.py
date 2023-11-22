5441faa8a9bd37991cd913c01d7d740c1d3d7eff
tanpv87 <tanpv87.cs@gmail.com>
2023-11-22 20:31:46 +0700

    change conđition in athena query

--- training.py
+++ training.py
@@ -18,7 +18,7 @@ print('hello world!')
 boto3.setup_default_session(region_name='us-east-1')
 
 # data_prepared_df = pd.read_parquet('s3://mlops-feature-stores/data-prepared_withoutpartitioning')
-data_prepared_df = wr.athena.read_sql_query(sql="select * from cloned_user_data where month >= '2022-06-01' and month <= '2022-10-31'", database='feature_stores')
+data_prepared_df = wr.athena.read_sql_query(sql="select * from cloned_user_data where monthly_report >= '2022-06-01' and monthly_report <= '2022-10-31'", database='feature_stores')
 
 data_prepared_df = data_prepared_df[(data_prepared_df.created_at >= '2022-06-01')
                                      & (data_prepared_df.created_at <= '2022-10-31')
