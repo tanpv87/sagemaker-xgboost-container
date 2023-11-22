8cebb57c63968089075feee1f562e4d4836f6af7
tanpv87 <tanpv87.cs@gmail.com>
2023-11-22 18:32:09 +0700

    using data without partitioning

--- training.py
+++ training.py
@@ -15,8 +15,8 @@ from botocore.exceptions import ClientError
 
 print('hello world!')
 
-# data_prepared_df = pd.read_parquet('s3://mlops-feature-stores/data-prepared')
-data_prepared_df = wr.athena.read_sql_query(sql="select * from cloned_user_data where month >= '2022-06-01' and month <= '2022-10-31'", database='feature_stores')
+data_prepared_df = pd.read_parquet('s3://mlops-feature-stores/data-prepared_withoutpartitioning')
+# data_prepared_df = wr.athena.read_sql_query(sql="select * from cloned_user_data where month >= '2022-06-01' and month <= '2022-10-31'", database='feature_stores')
 
 data_prepared_df = data_prepared_df[(data_prepared_df.created_at >= '2022-06-01')
                                      & (data_prepared_df.created_at <= '2022-10-31')
